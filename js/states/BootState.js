83e22a7a66d8a1a8c7267b8e003e0794fdb810f6
Barber, Keenan <Keenan.Barber@biworldwide.com>
2017-06-15 16:22:19 -0500

    Updates to BootState.

--- js/states/BootState.js
+++ js/states/BootState.js
@@ -22,3 +22,11 @@ FruitNinja.BootState.prototype.preload = function() {
 	"use strict"; 
 	this.load.text("level1", this.level_file);
 };
+
+FruitNinja.BootState.prototype.create = function() {
+	"use strict"; 
+	var level_text, level_data; 
+	level_text = this.game.cache.getText("level1");
+	level_data = JSON.parse(level_text);
+	this.game.state.start("LoadingState", true, false, level_data);
+}
